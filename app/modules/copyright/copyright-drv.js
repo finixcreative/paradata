6bbc640900fcdf4c3a72cb361bb1a6a497c92ed0
richard-symphony <richards@symphonycommerce.com>
2015-10-19 13:30:49 -0700

    Footer - copyright
    Update copyright and adjust modules

--- app/modules/copyright/copyright-drv.js
+++ app/modules/copyright/copyright-drv.js
@@ -2,7 +2,7 @@ angular.module('coreMod')
 	.directive('copyright', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/web/app/modules/copyright/copyright.html',
+			templateUrl: '/paradata/app/modules/copyright/copyright.html',
 			controller: 'ClientController',
 			controllerAs: 'client',
 		};
