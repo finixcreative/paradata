67d4aee02c61a52e788fd0aceed50d73787e59d3
richard-symphony <richards@symphonycommerce.com>
2015-10-19 09:17:36 -0700

    Social - update bindings
    Use ng-repeat to remove duclication

--- app/modules/social/social-drv.js
+++ app/modules/social/social-drv.js
@@ -2,7 +2,7 @@ angular.module('coreMod')
 	.directive('social', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/web/app/modules/social/social.html',
+			templateUrl: '/paradata/app/modules/social/social.html',
 			controller: 'ClientController',
 			controllerAs: 'client',
 		};
