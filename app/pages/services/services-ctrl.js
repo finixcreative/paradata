34e7b95abfdf6865e2a1ba0fe407b0859115b695
richard-symphony <richards@symphonycommerce.com>
2015-10-21 10:26:00 -0700

    Pages - placeholder markup
    Add subpages for each nav item

--- app/pages/services/services-ctrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('productsMod')
-	.controller('ProductsController', ['$scope', function($scope){
-		this.services = services;
-//		$scope.products.selected = {};
-//		$scope.update = function(order) {
-//			$scope.master = angular.copy(order);
-//		};
-	}]);
