d6462449fb3bfc35d1ba5629d1507b4154ea7178
richard-symphony <richards@symphonycommerce.com>
2015-10-23 08:50:41 -0700

    Routes - clean up
    Clean up routes mod

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,9 @@
 angular.module('paraData')
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
-/*			.when('/home', {
-				templateUrl: 'app/pages/home/home.html',
-			})
-*/			.when('/:page', {
+			.when('/:page', {
 				templateUrl: 'app/modules/chimera/chimera.html',
 				controller: 'SitemapController',
-				controllerAs: 'sitemap',
 			})
 			.otherwise({
 				redirectTo: '/home',
