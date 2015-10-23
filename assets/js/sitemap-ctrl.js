4b8ecfb34b738ab5defb219ada48cdfc08c8b603
richard-symphony <richards@symphonycommerce.com>
2015-10-23 08:34:47 -0700

    Sitemap - controller scope
    Add $ to scope

--- assets/js/sitemap-ctrl.js
+++ assets/js/sitemap-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('coreMod')
 	.controller('SitemapController', ['$scope', '$routeParams', function($scope, $routeParams){
 		this.sitemap = sitemap;
-		scope.page = 'app/pages/'+$routeParams.page+'/'+$routeParams.page+'.html';
+		$scope.page = 'app/pages/'+$routeParams.page+'/'+$routeParams.page+'.html';
 	}]);
