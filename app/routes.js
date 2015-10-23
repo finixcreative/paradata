de5e5a794d97de49384b3dde2d421d06401aaafb
richard-symphony <richards@symphonycommerce.com>
2015-10-23 08:39:50 -0700

    Routes - remove extraneous routes
    Merge all routes into Chimera engine

--- app/routes.js
+++ app/routes.js
@@ -1,33 +1,15 @@
 angular.module('paraData')
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
-			.when('/home', {
+/*			.when('/home', {
 				templateUrl: 'app/pages/home/home.html',
 			})
-			.when('/:page', {
+*/			.when('/:page', {
 				templateUrl: 'app/modules/chimera/chimera.html',
 				controller: 'SitemapController',
 				controllerAs: 'sitemap',
 			})
-/*			.when('/solutions', {
-				templateUrl: 'app/pages/solutions/solutions.html',
-			})
-			.when('/markets', {
-				templateUrl: 'app/pages/markets/markets.html',
-			})
-			.when('/technology', {
-				templateUrl: 'app/pages/technology/technology.html',
-			})
-			.when('/company', {
-				templateUrl: 'app/pages/company/company.html',
-			})
-			.when('/careers', {
-				templateUrl: 'app/pages/careers/careers.html',
-			})
-			.when('/blog', {
-				templateUrl: 'app/pages/blog/blog.html',
-			})
-*/			.otherwise({
+			.otherwise({
 				redirectTo: '/home',
 			});
 	}]);
