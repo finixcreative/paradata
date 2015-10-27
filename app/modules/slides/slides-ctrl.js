4afc1cca11b70e028d41b5ded55a0fde86278757
richard-symphony <richards@symphonycommerce.com>
2015-10-27 14:01:46 -0700

    Slides - controller syntax
    Add comma to separate dependencies from function

--- app/modules/slides/slides-ctrl.js
+++ app/modules/slides/slides-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('slidesMod')
-	.controller('SlidesController', ['$scope', 'Slides', 'Template' function($scope, Slides, Template){
+	.controller('SlidesController', ['$scope', 'Slides', 'Template', function($scope, Slides, Template){
 		$scope.images = Slides();
 		$scope.slider = Template();
 //		$scope.slider = 'app/modules/slides/'+$scope.slider+'.html';
