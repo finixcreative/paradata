5d0c290df2240aae7492658706da27dfd403488e
richard-symphony <richards@symphonycommerce.com>
2015-10-27 14:47:02 -0700

    Slides - factory scope
    Define scope for use in slides factory

--- app/modules/slides/slides-srv.js
+++ app/modules/slides/slides-srv.js
@@ -1,7 +1,7 @@
 angular.module('slidesMod', ['ngAnimate', 'ngTouch', 'ngResource'])
-	.factory('Slides', ['$resource', function($resource){
+	.factory('Slides', ['$resource', '$scope', function($resource, $scope){
 		return $resource('app/modules/slides/slides/:slide.json', {}, {
-			params: {slide: scope.images[scope.currentIndex]},
+			params: {slide: $scope.images[$scope.currentIndex]},
 			method: 'GET',
 			isArray: true
 		});
