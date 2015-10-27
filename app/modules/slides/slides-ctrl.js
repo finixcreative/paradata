05143e898bdbfd6cf4a08febaf58c1201ffa4ca9
richard-symphony <richards@symphonycommerce.com>
2015-10-27 13:55:21 -0700

    Slides - inject dependencies
    Add template & slide factories

--- app/modules/slides/slides-ctrl.js
+++ app/modules/slides/slides-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('slidesMod')
-	.controller('SlidesController', function($scope){
+	.controller('SlidesController', function($scope, 'Slides', 'Template'){
 		$scope.images = Slides();
 		$scope.slider = Template();
 //		$scope.slider = 'app/modules/slides/'+$scope.slider+'.html';
