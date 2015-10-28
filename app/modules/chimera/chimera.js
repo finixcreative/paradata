49d16471c801f70f5b89b6d700c87c60cd743e9e
richard-symphony <richards@symphonycommerce.com>
2015-10-28 10:30:56 -0700

    Revert "Slides - controller & factory"
    This reverts commit c22271efeeb8b1cc85a7849052642811fa44d675.

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -2,9 +2,5 @@
 
 angular.module('chimeraMod', [])
 	.controller('ChimeraController', ['$scope', '$routeParams', function($scope, $routeParams){
-		$scope.contents = 'app/pages/'+$routeParams.chimera+'/'+$routeParams.chimera+'.js';
-		$scope.template = 'app/pages/'+$routeParams.chimera+'/'+$routeParams.chimera+'.html';
-//		$scope.controller = 'app/modules/'+$routeParams.chimera+'/'+$routeParams.chimera+'-ctrl.js';
-//		$scope.directive = 'app/modules/'+$routeParams.chimera+'/'+$routeParams.chimera+'-drv.js';
-//		$scope.service = 'app/modules/'+$routeParams.chimera+'/'+$routeParams.chimera+'-srv.js';
+		$scope.chimera = 'app/pages/'+$routeParams.chimera+'/'+$routeParams.chimera+'.html';
 	}]);
--- app/modules/slides/slides-srv.js
+++ app/modules/slides/slides-srv.js
@@ -1,10 +1,8 @@
 angular.module('slidesMod', ['ngAnimate', 'ngTouch', 'ngResource'])
 	.factory('Slides', ['$resource', '$scope', function($resource, $scope){
 		return $resource('app/modules/slides/slides/:slide.json', {}, {
-			load: {
-				params: {slide: 'slides'},
-				method: 'GET',
-				isArray: true
-			}
+			params: {slide: $scope.images[$scope.currentIndex]},
+			method: 'GET',
+			isArray: true
 		});
 	}]);
