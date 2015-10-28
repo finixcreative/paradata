88d79b985d11b14bd11b4e12250e9aca0933b621
richard-symphony <richards@symphonycommerce.com>
2015-10-28 10:32:19 -0700

    Revert "Slides - controller images"
    This reverts commit cba96e0554aa2b585ddc9e1efd25cf8871508791.

--- app/modules/slides/slides-ctrl.js
+++ app/modules/slides/slides-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('slidesMod')
 	.controller('SlidesController', function($scope){
-		$scope.images = '=';
-		$scope.template = 'app/modules/slides/'+$scope.template+'.html';
+//		$scope.images = images;
+		$scope.template = 'app/modules/slides/'+$scope.template+'.html';		
 	});
