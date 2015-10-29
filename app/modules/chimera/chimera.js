3469be0eaf467464852a81c73a63190b9965d372
richard-symphony <richards@symphonycommerce.com>
2015-10-29 08:47:38 -0700

    Chimera - content data
    Create content database

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -4,6 +4,6 @@ angular.module('chimeraMod', [])
 	.controller('ChimeraController', ['$scope', '$routeParams', function($scope, $routeParams){
 		var chimera = $routeParams.chimera;
 		var root = 'app/pages/';
-		$scope.page = root+chimera+'/'+chimera+'.js';
-		$scope.page.template = root+chimera+'/'+chimera+'.html';
+		$scope.page = root+chimera+'/'+chimera+'.json';
+//		$scope.page.template = root+chimera+'/'+chimera+'.html';
 	}]);
