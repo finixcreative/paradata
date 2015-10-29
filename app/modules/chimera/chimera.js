cfeb66dec6e08c84e311bd83458e78187df194bb
richard-symphony <richards@symphonycommerce.com>
2015-10-29 09:02:42 -0700

    Chimera - non-blocking import
    Fetch async data with non-blocking function

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -4,6 +4,8 @@ angular.module('chimeraMod', [])
 	.controller('ChimeraController', ['$scope', '$routeParams', function($scope, $routeParams){
 		var chimera = $routeParams.chimera;
 		var root = 'app/pages/';
-		$scope.page = root+chimera+'/'+chimera+'.json';
-		$scope.page.template = root+chimera+'/'+chimera+'.html';
+		$scope.page = function(){
+			$scope = root+chimera+'/'+chimera+'.json';
+		};
+//		$scope.page.template = root+chimera+'/'+chimera+'.html';
 	}]);
