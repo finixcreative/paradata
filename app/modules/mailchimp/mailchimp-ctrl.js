248e296472b935293bcdd018e1527d4fba55e939
richard-symphony <richards@symphonycommerce.com>
2015-11-02 14:21:02 -0800

    Mailchimp - clean up
    Clean up controller, view, and index files

--- app/modules/mailchimp/mailchimp-ctrl.js
+++ app/modules/mailchimp/mailchimp-ctrl.js
@@ -2,9 +2,5 @@ angular.module('coreMod')
 	.controller('MailchimpController', function($scope){
 		$scope.popover = function(){
 			$('div.email-signup').toggleClass('visible-block');
-//			document.getElementsByClassName('email-signup').classList.add('visible-block');
 		};
-/*		$scope.close = function(){
-			document.getElementsByClassName('email-signup').classList.remove('visible-block');
-		}; */
 	});
