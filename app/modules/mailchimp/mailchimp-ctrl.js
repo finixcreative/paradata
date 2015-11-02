fc44c23634c5b6c17cf42fec776fa43a27fdb870
richard-symphony <richards@symphonycommerce.com>
2015-11-02 10:45:10 -0800

    Mailchimp - jQuery
    Use jQuery method to toggleClass on popover form

--- app/modules/mailchimp/mailchimp-ctrl.js
+++ app/modules/mailchimp/mailchimp-ctrl.js
@@ -1,9 +1,10 @@
 angular.module('coreMod')
 	.controller('MailchimpController', function($scope){
-		$scope.open = function(){
-			document.getElementsByClassName('email-signup').classList.add('visible-block');
+		$scope.popover = function(){
+			$('div.email-signup').toggleClass('visible-block');
+//			document.getElementsByClassName('email-signup').classList.add('visible-block');
 		};
-		$scope.close = function(){
+/*		$scope.close = function(){
 			document.getElementsByClassName('email-signup').classList.remove('visible-block');
-		};
+		}; */
 	});
