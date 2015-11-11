7cb3745c68e85cddad8ca00ce56dd138168dd578
rsmith-finix <rsmith@finixcreative.com>
2015-11-11 12:57:26 -0800

    Slider - slide change
    Inject $timeout into slide change function

--- app/modules/slides/slide-change.js
+++ app/modules/slides/slide-change.js
@@ -1,4 +1,4 @@
-function slideChange (scope, elem, attrs){
+function slideChange (scope, elem, attrs, $timeout){
 	scope.currentIndex = 0;
 	for(var i = 0; i < scope.images.length; i++){
 		scope.images.index = i;
