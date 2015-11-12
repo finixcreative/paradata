216870ff46408c84349144e0d2ff862f21dd6497
rsmith-finix <rsmith@finixcreative.com>
2015-11-12 10:36:47 -0800

    Chimera - controller
    Add http get to controller to pull dynamic content from json data

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -1,9 +1,17 @@
 // Copyright 2015 Finix. All rights reserved.
 
 angular.module('chimeraMod', [])
-	.controller('ChimeraController', ['$scope', '$routeParams', function($scope, $routeParams){
+	.controller('ChimeraController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 		var chimera = $routeParams.chimera;
 		var root = 'app/pages/';
-		$scope.page = root+chimera+'/'+chimera+'.json';
+		var page = root+chimera+'/'+chimera+'.json';
 		$scope.template = root+chimera+'/'+chimera+'.html';
+		$http.get(page).
+			success(function(data, status, headers, config) {
+				$scope.contents = data;
+			}).
+			error(function(data, status, headers, config) {
+				console.log(error)
+				// log error
+			});
 	}]);
