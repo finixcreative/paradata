c124c74649bfa3448f66d77e42a911624826ad6e
rsmith-finix <rsmith@finixcreative.com>
2015-11-13 21:09:12 -0800

    Chimera - unit test refactor
    Rewrite tests to use if/else

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -8,9 +8,12 @@ angular.module('chimeraMod', [])
 		$scope.template = root+chimera+'/'+chimera+'.html';
 		$http.get(page).success(function(data, status, headers, config){
 			$scope.contents = data;
+			if($scope.contents.length > 0){
+				console.log("Contents detected: length is " + $scope.contents.length);
+			} else {
+				console.log("Error detected: length is " + $scope.contents.length);				
+			};
 		});
-		console.assert($scope.contents.length < 1, "Error detected: length is " + $scope.contents.length);
-		console.assert($scope.contents.length > 0, "Contents detected: length is " + $scope.contents.length);
 		console.log("Page: " + chimera);
 		console.log("Template: " + $scope.template);
 		console.log("Contents: " + $scope.contents);
