b79cb9f547da9b97d28d0996b73931d4ae84a53b
rsmith-finix <rsmith@finixcreative.com>
2015-11-13 16:25:32 -0800

    Chimera - log page
    Add page to console log queue

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -13,7 +13,7 @@ angular.module('chimeraMod', [])
 		});
 		console.assert($scope.contents.length < 1, "Error detected: length is " + $scope.contents.length);
 		console.assert($scope.contents.length > 0, "Contents detected: length is " + $scope.contents.length);
-		console.log("Chimera: " + chimera);
+		console.log("Page: " + chimera);
 		console.log("Template: " + $scope.template);
 		console.log("Contents: " + $scope.contents);
 	}]);
