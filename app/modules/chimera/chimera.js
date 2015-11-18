d388c212862b5900fa02c9888ffd8307c70ce6e3
rsmith-finix <rsmith@finixcreative.com>
2015-11-18 10:23:48 -0800

    Chimera - pageData
    Use var name “pagedata” to avoid namespace conflicts

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -3,11 +3,11 @@
 angular.module('chimeraMod', [])
 	.controller('ChimeraController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 		var chimera = $routeParams.chimera;
-		var page = 'app/contents/'+chimera+'.json';
+		var pageData = 'app/contents/'+chimera+'.json';
 		$scope.template = 'app/pages/'+chimera+'/'+chimera+'.html';
 		$scope.contents = [];
 		$scope.errors = [];
-		$http.get(page).then(function success(response){
+		$http.get(pageData).then(function success(response){
 			//success callbacks
 			$scope.page = response.data;
 			console.log(
