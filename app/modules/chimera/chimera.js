3258d718afbe9865c1c39b78ed0e317544b9e576
rsmith-finix <rsmith@finixcreative.com>
2015-11-18 10:15:49 -0800

    Chimera - page
    Set scope.page to response.data

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -3,26 +3,25 @@
 angular.module('chimeraMod', [])
 	.controller('ChimeraController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 		var chimera = $routeParams.chimera;
-		var root = 'app/pages/';
 		var page = 'app/contents/'+chimera+'.json';
-		$scope.template = root+chimera+'/'+chimera+'.html';
+		$scope.template = 'app/pages/'+chimera+'/'+chimera+'.html';
 		$scope.contents = [];
 		$scope.errors = [];
 		$http.get(page).then(function success(response){
 			//success callbacks
-			$scope.contents = response.data;
+			$scope.page = response.data;
 			console.log(
 				"Chimera: " + chimera + "\n",
 				"Page URL: " + page + "\n",
 				"Template URL: " + $scope.template + "\n",
-				"Page Name: " + $scope.contents.config.title + "\n",
-				"Contents: " + $scope.contents.contents
+				"Page Name: " + $scope.page.config.title + "\n",
+				"Contents: " + $scope.page.contents
 			);
-			for(var i = 0; i < $scope.contents.contents.length; i++){
+			for(var i = 0; i < $scope.page.contents.length; i++){
 				console.log(
-					"Widget " + (i + 1) + " of " + $scope.contents.contents.length + "\n",
-					"-> type " + $scope.contents.contents[i].config.type + "\n",
-					"-> contains " + $scope.contents.contents[i].contents.length + " objects."
+					"Widget " + (i + 1) + " of " + $scope.page.contents.length + "\n",
+					"-> type " + $scope.page.contents[i].config.type + "\n",
+					"-> contains " + $scope.page.contents[i].contents.length + " components."
 				);
 			};
 		}, function error(response){
