434d529e2ad0aa7e972ceb3fe797cefc758e8a1a
rsmith-finix <rsmith@finixcreative.com>
2015-11-22 23:55:46 -0800

    Chimera - contents
    Set scope var to “contents”

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -9,30 +9,30 @@ angular.module('chimeraMod', [])
 		$scope.errors = [];
 		$http.get(pageData).then(function success(response){
 			//success callbacks
-			$scope.content = response.data;
+			$scope.contents = response.data;
 			console.log(
 				"Chimera: " + chimera + "\n",
 				"Page URL: " + pageData + "\n",
 				"Template URL: " + $scope.template + "\n",
-				"Page Name: " + $scope.content.config.title + "\n",
-				"Contents: " + $scope.content.contents
+				"Page Name: " + $scope.contents.config.title + "\n",
+				"Contents: " + $scope.contents.contents
 			);
-			for(var i = 0; i < $scope.content.contents.length; i++){
+			for(var i = 0; i < $scope.contents.contents.length; i++){
 				console.log(
-					"Widget " + (i + 1) + " of " + $scope.content.contents.length + "\n",
-					"-> type " + $scope.content.contents[i].config.type + "\n",
-					"-> contains " + $scope.content.contents[i].contents.length + " components."
+					"Widget " + (i + 1) + " of " + $scope.contents.contents.length + "\n",
+					"-> type " + $scope.contents.contents[i].config.type + "\n",
+					"-> contains " + $scope.contents.contents[i].contents.length + " components."
 				);
 			};
 		}, function error(response){
 			//error callbacks
-			$scope.content = response.data;
+			$scope.contents = response.data;
 			$scope.errors = response.error;
 			console.log(
 				"Chimera: " + chimera + "\n",
 				"Page URL: " + pageData + "\n",
 				"Template URL: " + $scope.template + "\n",
-				"Contents: " + $scope.content.contents + "\n",
+				"Contents: " + $scope.contents.contents + "\n",
 				"Errors: " + $scope.errors
 			);
 		});
