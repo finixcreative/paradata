b494eca8f1a9e39177652455a07e65dbdde10f58
rsmith-finix <rsmith@finixcreative.com>
2015-12-06 12:38:13 -0800

    Chimera - log specs
    Clean up console log output format

--- app/modules/chimera/chimera.js
+++ app/modules/chimera/chimera.js
@@ -18,17 +18,18 @@ angular.module('chimeraMod', [])
 			);
 			for(var i = 0; i < $scope.contents.length; i++){
 				console.log(
-					"Page Name: " + $scope.contents[i].config.title + "\n",
+					"Name: " + $scope.contents[i].config.title + "\n",
+					"Type: " + $scope.contents[i].config.type + "\n",
+					$scope.contents[i].config.type + " " + (i + 1) + " of " + $scope.contents.length + "\n",
 					"Content Level 2: " + $scope.contents[i].contents + "\n",
-					"Page : " + (i + 1) + " of " + $scope.contents.length + "\n",
-					"-> type " + $scope.contents[i].config.type + "\n",
 					"-> contains " + $scope.contents[i].contents.length + " widgets."
 				);
 				for(var j = 0; j < $scope.contents[i].contents.length; j++){
 					console.log(
-						"Widget " + (j + 1) + " of " + $scope.contents[i].contents.length + "\n",
+						"Name: " + $scope.contents[i].contents[j].config.title + "\n",
+						"Type: " + $scope.contents[i].contents[j].config.type + "\n",
+						$scope.contents[i].contents[j].config.type + " " + (j + 1) + " of " + $scope.contents[i].contents.length + "\n",
 						"Content Level 3: " + $scope.contents[i].contents[j].contents + "\n",
-						"-> type " + $scope.contents[i].contents[j].config.type + "\n",
 						"-> contains " + $scope.contents[i].contents[j].contents.length + " components."
 					);
 				};
