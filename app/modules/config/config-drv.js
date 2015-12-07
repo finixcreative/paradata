0af3dcdd9517812f51ea00b9261b264cd8e6379f
rsmith-finix <rsmith@finixcreative.com>
2015-12-07 10:47:18 -0800

    Text Modules - config directive
    Create config directive to standardize all content editors

---
+++ app/modules/config/config-drv.js
@@ -0,0 +1,13 @@
+angular.module('coreMod')
+	.directive('configtext', function(){
+		return {
+			restrict: 'E',
+			templateUrl: '/paradata/app/modules/config/configtext.html'
+		};
+	})
+	.directive('configimg', function(){
+		return {
+			restrict: 'E',
+			templateUrl: '/paradata/app/modules/config/configimg.html'
+		};
+	});
