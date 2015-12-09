28c9575f2db2b879598a6c8555afdc45817b22ab
rsmith-finix <rsmith@finixcreative.com>
2015-12-08 23:01:25 -0800

    Config - controller edit
    Set edit initial value to “false”

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,6 +1,4 @@
 angular.module('coreMod')
 	.controller('ConfigController', function($scope){
-		$scope.edit = function(){
-			$('form.config').toggleClass('visible-block');
-		};
+		$scope.edit = false;
 	});
