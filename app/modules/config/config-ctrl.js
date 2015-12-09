bc9a7ad96f26494d1cd85c44953f00d2516459ca
rsmith-finix <rsmith@finixcreative.com>
2015-12-09 13:28:11 -0800

    Config - scope object
    Build values into scope object

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,5 +1,7 @@
 angular.module('coreMod')
 	.controller('ConfigController', function($scope){
-		$scope.config = false;
-		$scope.edit = false;
+		$scope = {
+			"config": false,
+			"edit": false
+		};
 	});
