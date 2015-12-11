697f82f2f164f9295f8d56edb89ea69e4079c2a5
rsmith-finix <rsmith@finixcreative.com>
2015-12-11 14:16:10 -0800

    Config - clean up controller
    Tidy up code

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,14 +1,14 @@
 angular.module('coreMod')
 	.controller('ConfigController', function($scope){
-		var config = false;
 		$scope.edit = false;
-		$scope.configPanel = function(){
+		var config = false;
+		var configPanel = function(){
 			if(config == false){
 				config = true;
+				return console.log("Config is active");
 			} else {
 				config = false;
+				return console.log("Config is inactive");
 			};
-			return config;
-			return console.log("Config is " + config);
 		};
 	});
