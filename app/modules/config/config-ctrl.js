935d8c308e31caf31f215767261460e6e2324315
rsmith-finix <rsmith@finixcreative.com>
2015-12-11 14:24:23 -0800

    Config - toggle click
    Change the function on click toggle

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -2,13 +2,4 @@ angular.module('coreMod')
 	.controller('ConfigController', function($scope){
 		$scope.config = false;
 		$scope.edit = false;
-		$scope.configPanel = function(){
-			if($scope.config == false){
-				$scope.config = true;
-			} else {
-				$scope.config = false;
-			};
-			return $scope.config;
-			return console.log("Config is " + $scope.config);
-		};
 	});
