b0e849fdc699e46101ddcf387792ef80c83c9bcc
rsmith-finix <rsmith@finixcreative.com>
2015-12-11 12:19:18 -0800

    Config - controller scope
    Declare scope

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('coreMod')
-	.controller('ConfigController', function($scope){
+	.controller('ConfigController', [$scope, function($scope){
 		$scope.config = false;
 		$scope.edit = false;
-	});
+	}]);
