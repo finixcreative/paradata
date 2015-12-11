77a7515210a01cc90ac95b2c13dfe0c4844a7410
rsmith-finix <rsmith@finixcreative.com>
2015-12-11 12:28:18 -0800

    Config - panel function
    Add output to test function

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -2,4 +2,12 @@ angular.module('coreMod')
 	.controller('ConfigController', function($scope){
 		$scope.config = false;
 		$scope.edit = false;
+		$scope.configPanel = function(){
+			if($scope.config == false){
+				$scope.config = true;
+			} else {
+				$scope.config = false;
+			};
+			return console.log("Config is " + $scope.config);
+		};
 	});
