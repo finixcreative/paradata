196100b8bae8126ad26c10d3d1a56481216b48c4
rsmith-finix <rsmith@finixcreative.com>
2015-12-11 14:09:35 -0800

    Config - global variable
    Set global variable within controller

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,13 +1,14 @@
 angular.module('coreMod')
 	.controller('ConfigController', function($scope){
-		$scope.config = false;
+		var config = false;
 		$scope.edit = false;
 		$scope.configPanel = function(){
-			if($scope.config == false){
-				$scope.config = true;
+			if(config == false){
+				config = true;
 			} else {
-				$scope.config = false;
+				config = false;
 			};
-			return console.log("Config is " + $scope.config);
+			return config;
+			return console.log("Config is " + config);
 		};
 	});
