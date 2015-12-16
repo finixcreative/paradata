9737e8bdd85a465ae7f54fb803a43a1e24912529
rsmith-finix <rsmith@finixcreative.com>
2015-12-16 10:23:44 -0800

    Config - if/else
    Refactor controller toggle function to if/else statement

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -2,14 +2,11 @@ angular.module('coreMod')
 	.controller('ConfigController', function($scope){
 		var configure = false;
 		function configureToggle(){
-			/*	
 			if(configure == false){
 				configure = true;
 			} else {
 				configure = false;
 			};
-			*/
-			configure = !configure;
 			console.log(configure);
 		};
 		$scope.edit = false;
