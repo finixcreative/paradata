dd9a4d70e6a0e228093755f9f2332e20df080128
rsmith-finix <rsmith@finixcreative.com>
2015-12-18 13:32:51 -0800

    Config - scope object
    Refactor scope value from array into object

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,14 +1,14 @@
 angular.module('coreMod')
 	.controller('ConfigController', function($scope){
-		$scope = [
-			configure = true,
-			edit = false,
-			configToggle = function(){
+		$scope = {
+			configure: true,
+			edit: false,
+			configToggle: function(){
 				configure = !configure;
 				console.log(configure);
 				return configure;
 			}
-		];
+		};
 		console.log(
 			$scope.configure + "\n",
 			$scope.edit
