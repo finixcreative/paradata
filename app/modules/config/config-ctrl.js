125fd724192924e1615fc70722e409fc9fd24845
rsmith-finix <rsmith@finixcreative.com>
2015-12-18 23:37:55 -0800

    Config - hoist configToggle
    Move configToggle to top-level of controller

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,17 +1,18 @@
 angular.module('coreMod')
 	.controller('ConfigController', function($scope){
+		var configToggle = [],
 		$scope = {
 			configure: true,
-			edit: false,
-			configToggle: function(){
-				$scope.configure = !$scope.configure;
-				console.log($scope.configure);
-				return $scope.configure;
-			}
+			edit: false
+		};
+		configToggle = function(){
+			$scope.configure = !$scope.configure;
+			console.log($scope.configure);
+			return $scope.configure;
 		};
 		console.log(
 			"configure: " + $scope.configure + "\n",
 			"edit: " + $scope.edit + "\n",
-			"configToggle: " + $scope.configToggle()
+			"configToggle: " + configToggle()
 		);
 	});
