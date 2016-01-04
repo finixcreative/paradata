17fead90c48eb374e572cfe1fea9dda0386bfee7
rsmith-finix <rsmith@finixcreative.com>
2016-01-04 00:55:21 -0800

    Config - dynamic configtype
    Set dynamic configtype with hydra controller

--- app/modules/hydra/hydra-ctrl.js
+++ app/modules/hydra/hydra-ctrl.js
@@ -3,6 +3,17 @@
 angular.module('hydraMod')
 	.controller('HydraController', function($scope){
 		$scope.edit = false;
+		$scope.configtype = function(){
+			if (content.config.type == "h1" || "h2" || "h3" || "h4" || "h5" || "h6"){
+				return "configheadline";
+			} else if (content.config.type == "p"){
+				return "configtext";
+			} else if (content.config.type == "img"){
+				return "configimg";
+			} else {
+				return null;
+			};
+		};
 		/*
 		$scope.saveChanges = function(){
 			var data = $.param({
