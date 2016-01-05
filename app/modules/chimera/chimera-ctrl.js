3865e70446fff1193f628f12afd4cb9a819037b9
rsmith-finix <rsmith@finixcreative.com>
2016-01-05 00:03:11 -0800

    Chimera - scope variable
    Add “$scope” to configtype function variable

--- app/modules/chimera/chimera-ctrl.js
+++ app/modules/chimera/chimera-ctrl.js
@@ -10,13 +10,13 @@ angular.module('chimeraMod', [])
 		$scope.configure = true;
 		$scope.configtype = function($scope){
 			console.log("* Config Panel *");
-			if (contents.config.type == "h1" || "h2" || "h3" || "h4" || "h5" || "h6"){
+			if ($scope.contents.config.type == "h1" || "h2" || "h3" || "h4" || "h5" || "h6"){
 				console.log("Config Headline");
 				return "configheadline";
-			} else if (contents.config.type == "p"){
+			} else if ($scope.contents.config.type == "p"){
 				console.log("Config Text");
 				return "configtext";
-			} else if (contents.config.type == "img"){
+			} else if ($scope.contents.config.type == "img"){
 				console.log("Config Image");
 				return "configimg";
 			} else {
