5ecef948660ab428c1955034eebbfc69b4ebb047
rsmith-finix <rsmith@finixcreative.com>
2016-01-04 23:19:07 -0800

    Chimera - config variable
    Update configtype function

--- app/modules/chimera/chimera-ctrl.js
+++ app/modules/chimera/chimera-ctrl.js
@@ -10,13 +10,13 @@ angular.module('chimeraMod', [])
 		$scope.configure = true;
 		$scope.configtype = function($scope){
 			console.log("* Config Panel *");
-			if ($scope.content.config.type == "h1" || "h2" || "h3" || "h4" || "h5" || "h6"){
+			if ($scope.config.type == "h1" || "h2" || "h3" || "h4" || "h5" || "h6"){
 				console.log("Config Headline");
 				return "configheadline";
-			} else if ($scope.content.config.type == "p"){
+			} else if ($scope.config.type == "p"){
 				console.log("Config Text");
 				return "configtext";
-			} else if ($scope.content.config.type == "img"){
+			} else if ($scope.config.type == "img"){
 				console.log("Config Image");
 				return "configimg";
 			} else {
