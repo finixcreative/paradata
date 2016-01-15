73dd98781ed98b640a171c758c3a7ee7092e3d21
rsmith-finix <rsmith@finixcreative.com>
2016-01-15 12:47:09 -0800

    Sitemap - controller
    Update controller to lazy-load the sitemap json file

--- assets/js/sitemap-ctrl.js
+++ assets/js/sitemap-ctrl.js
@@ -1,4 +1,22 @@
 angular.module('coreMod')
 	.controller('SitemapController', function(){
-		this.sitemap = 'assets/js/sitemap.json';
-	});
+		var filepath = 'assets/js/sitemap.json';
+		$scope.sitemap = [];
+		$http.get(filepath).then(
+			function success(response){
+				//success callbacks
+				$scope.sitemap = response.data;
+				console.log("Sitemap: " + $scope.sitemap);
+			},
+			function error(response){
+				//error callbacks
+				$scope.sitemap = response.data;
+				$scope.errors = response.error;
+				console.log(
+					"Sitemap: " + $scope.sitemap + "\n",
+					"Errors: " + $scope.errors
+				);
+			}
+		);
+	})
+;
