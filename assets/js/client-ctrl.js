a672f1a623052040d8232aa4c389eeaf7b81fa0e
rsmith-finix <rsmith@finixcreative.com>
2016-01-17 23:43:13 -0800

    Client - controller
    Update controller to lazy-load the client model

--- assets/js/client-ctrl.js
+++ assets/js/client-ctrl.js
@@ -1,4 +1,22 @@
 angular.module('coreMod', [])
-	.controller('ClientController', function(){
-		this.client = client;
-	});
+	.controller('ClientController', ['$scope', '$http', function($scope, $http){
+		var filepath = 'assets/js/client.json';
+		$scope.client = [];
+		$http.get(filepath).then(
+			function success(response){
+				//success callbacks
+				$scope.client = response.data;
+				console.log("Client: " + $scope.client);
+			},
+			function error(response){
+				//error callbacks
+				$scope.client = response.data;
+				$scope.errors = response.error;
+				console.log(
+					"Client: " + $scope.client + "\n",
+					"Errors: " + $scope.errors
+				);
+			}
+		);
+	}])
+;
