e250142e30a51095339b6d371341a20681204c67
rsmith-finix <rsmith@finixcreative.com>
2016-01-20 09:05:19 -0800

    Home - slide-change
    Update variables in slideChange function

--- app/modules/slides/slide-change.js
+++ app/modules/slides/slide-change.js
@@ -1,45 +1,45 @@
-function slideChange (scope, elem, attrs, $timeout){
-	scope.currentIndex = 0;
-	for(var i = 0; i < scope.images.length; i++){
-		scope.images.index = i;
+function slideChange ($scope, $elem, $attrs, $timeout){
+	$scope.currentIndex = 0;
+	for(var i = 0; i < $scope.slides.length; i++){
+		$scope.slides.index = i;
 	};
-	scope.selectIndex = function(index){
-		scope.currentIndex = index;
+	$scope.selectIndex = function(index){
+		$scope.currentIndex = index;
 	};
-	scope.isCurrentSlideIndex = function (index) {
-		return scope.currentIndex === index;
+	$scope.isCurrentSlideIndex = function (index) {
+		return $scope.currentIndex === index;
 	};
-	scope.next = function(){
-		scope.currentIndex < scope.images.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
+	$scope.next = function(){
+		$scope.currentIndex < $scope.slides.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
 	};
-	scope.prev = function(){
-		scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
+	$scope.prev = function(){
+		$scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.slides.length - 1;
 	};
-	scope.$watch('currentIndex', function() {
-		scope.images.forEach(function(image) {
-			image.visible = false;
+	$scope.$watch('currentIndex', function() {
+		$scope.slides.forEach(function(slide) {
+			slide.visible = false;
 		});
-		scope.images[scope.currentIndex].visible = true;
+		$scope.slides[$scope.currentIndex].visible = true;
 	});
 	/*
 	var autoslide = function(){
-		var next = scope.next();
+		var next = $scope.next();
 		$timeout(next, 5000);
 	};
 	autoslide();
-	scope.$on('$destroy', function(){
+	$scope.$on('$destroy', function(){
 		$timeout.cancel(timer);
 	});
 	*/
 	var timer;
 	var sliderFunc = function(){
 		timer = $timeout(function(){
-			scope.next();
+			$scope.next();
 			timer = $timeout(sliderFunc, 5000);
 		}, 5000);
 	};
 	sliderFunc();
-	scope.$on('$destroy', function() {
+	$scope.$on('$destroy', function() {
 		$timeout.cancel(timer); // when the scope is getting destroyed, cancel the timer
 	});
 };
