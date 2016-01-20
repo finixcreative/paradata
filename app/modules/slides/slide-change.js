b8d83893b63d9213dda23c1aa2580adef0368334
rsmith-finix <rsmith@finixcreative.com>
2016-01-20 14:13:46 -0800

    Sync
    Sync

--- app/modules/slides/slide-change.js
+++ app/modules/slides/slide-change.js
@@ -1,25 +1,33 @@
-function slideChange (scope, elem, attrs, $timeout){
+function slideChange(scope, $elem, $attrs, $timeout){
 	scope.currentIndex = 0;
-	for(var i = 0; i < scope.images.length; i++){
-		scope.images.index = i;
+	for(var i = 0; i < scope.slides.length; i++){
+		scope.slides.index = i;
 	};
 	scope.selectIndex = function(index){
 		scope.currentIndex = index;
 	};
-	scope.isCurrentSlideIndex = function (index) {
+	scope.isCurrentSlideIndex = function(index) {
 		return scope.currentIndex === index;
 	};
 	scope.next = function(){
-		scope.currentIndex < scope.images.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
+		scope.currentIndex < scope.slides.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
 	};
 	scope.prev = function(){
-		scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
+		scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.slides.length - 1;
 	};
-	scope.$watch('currentIndex', function() {
-		scope.images.forEach(function(image) {
-			image.visible = false;
+	scope.$watch('currentIndex', function(){
+		scope.slides.forEach(function(slide){
+			console.log(scope.slide);
+			console.log(scope.slide.visible);
+			scope.slide.visible = false;
+			console.log(scope.slide.visible);
 		});
-		scope.images[scope.currentIndex].visible = true;
+		console.log(scope.currentIndex);
+		console.log(scope.slides);
+		console.log(scope.slides[scope.currentIndex]);
+		console.log(scope.slides[scope.currentIndex].visible);
+		scope.slides[scope.currentIndex].visible = true;
+		console.log(scope.slides[scope.currentIndex].visible);
 	});
 	/*
 	var autoslide = function(){
