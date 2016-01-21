c9f2f1ae53f4abd97eed199c18b3daf84137599e
rsmith-finix <rsmith@finixcreative.com>
2016-01-20 16:24:33 -0800

    Home - slides get
    Move scope functions inside get callback

--- app/modules/slides/slides-ctrl.js
+++ app/modules/slides/slides-ctrl.js
@@ -16,6 +16,22 @@ angular.module('slidesMod')
 						"Slide visible: " + $scope.slides[i].visible
 					);
 				};
+				$scope.selectIndex = function(index){
+					$scope.currentIndex = index;
+				};
+				$scope.isCurrentSlideIndex = function(index) {
+					return $scope.currentIndex === index;
+				};
+				$scope.next = function(){
+					$scope.currentIndex < $scope.slides.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+				};
+				$scope.prev = function(){
+					$scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.slides.length - 1;
+				};
+				$scope.$watch('currentIndex', function(){
+					$scope.slides[$scope.currentIndex].visible = true;
+					console.log($scope.slides[$scope.currentIndex].visible);
+				});
 			},
 			function error(response){
 				//error callbacks
@@ -27,39 +43,5 @@ angular.module('slidesMod')
 				);
 			}
 		);
-		$scope.selectIndex = function(index){
-			$scope.currentIndex = index;
-		};
-		$scope.isCurrentSlideIndex = function(index) {
-			return $scope.currentIndex === index;
-		};
-		$scope.next = function(){
-			$scope.currentIndex < $scope.slides.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
-		};
-		$scope.prev = function(){
-			$scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.slides.length - 1;
-		};
-		$scope.$watch('currentIndex', function(){
-			$scope.slides[$scope.currentIndex].visible = true;
-			console.log($scope.slides[$scope.currentIndex].visible);
-		});
-		/*
-		var autoslide = function(){
-			var next = $scope.next();
-			$timeout(next, 5000);
-		};
-		autoslide();
-		var timer;
-		var sliderFunc = function(){
-			timer = $timeout(function(){
-				$scope.next();
-				timer = $timeout(sliderFunc, 5000);
-			}, 5000);
-		};
-		sliderFunc();
-		$scope.$on('$destroy', function(){
-			$timeout.cancel(timer); // when the $scope is getting destroyed, cancel the timer
-		});
-		*/
 	}])
 ;
