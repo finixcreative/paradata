088023500aef49669a7a1631b090abe5a21641ad
rsmith-finix <rsmith@finixcreative.com>
2016-01-21 15:06:08 -0800

    Salesforce - integrate lead capture
    Add directive, controller, and template along with supporting code
    updates in Home

---
+++ app/modules/salesforce/salesforce-ctrl.js
@@ -0,0 +1,6 @@
+angular.module('coreMod')
+	.controller('SalesforceController', function($scope){
+		$scope.popover = function(){
+			$('div.salesforce-form').toggleClass('visible-block');
+		};
+	});
